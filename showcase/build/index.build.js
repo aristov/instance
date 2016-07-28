73f10d0187c15a699929dc651fd8b3a7c4d1a0f5
aristov <vv.aristov@gmail.com>
2016-07-29 01:48:43 +0300

    fake rebuild

--- showcase/build/index.build.js
+++ showcase/build/index.build.js
@@ -39,7 +39,6 @@
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
 /******/ })
-/************************************************************************/
 /******/ ([
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
